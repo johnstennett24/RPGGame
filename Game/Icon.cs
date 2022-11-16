fe914ce3a13b65f274716e1d26d860a2799100c6
johnstennett24 <93450112+johnstennett24@users.noreply.github.com>
2022-11-15 21:59:03 -0700

    started project and added background and key inputs

---
+++ Game/Icon.cs
@@ -0,0 +1,41 @@
+using Godot;
+
+namespace Rpg8bit
+{
+	public class Icon : Sprite
+	{
+		// Declare member variables here. Examples:
+		// private int a = 2;
+		// private string b = "text";
+
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+			
+		}
+		
+		// public override void _Process(float delta)
+		// {
+		// 	const float amount = 5;
+		// 	if (Input.IsKeyPressed((int)KeyList.W))
+		// 	{
+		// 		Position += new Vector2(0, -amount);
+		// 	}
+		// 	if (Input.IsKeyPressed((int)KeyList.S))
+		// 	{
+		// 		Position += new Vector2(0, amount);
+		// 	}
+		//
+		// 	if (Input.IsKeyPressed((int)KeyList.A))
+		// 	{
+		// 		Position += new Vector2(-amount, 0);
+		// 	}
+		//
+		// 	if (Input.IsKeyPressed((int)KeyList.D))
+		// 	{
+		// 		Position += new Vector2(amount, 0);
+		// 	}
+		// }
+	
+	}
+}
